107fcf58fdc580f8da5ac98edd67a6ec0742a826
mohamed abd elaziz <61313695+mohamed-abdelaziz721@users.noreply.github.com>
2021-08-27 23:24:45 +0200

    Relative Layout similar to FB Post

---
+++ app/src/main/java/com/example/realative_layout_fb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.realative_layout_fb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
